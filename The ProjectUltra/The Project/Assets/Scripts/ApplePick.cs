dbb101443bdad740f18e899a71411c3398c318f2
Brandon Ashworth <Brandon.Ashworth.9@gmail.com>
2016-08-07 22:04:28 +0800

    Initial commit

---
+++ The ProjectUltra/The Project/Assets/Scripts/ApplePick.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ApplePick : MonoBehaviour {
+	public int value;
+	private GameObject player;
+	private PlayerInventory playerInventory;
+
+
+	// Use this for initialization
+	void Awake () {
+		player = GameObject.FindGameObjectWithTag ("Player");
+		playerInventory = player.GetComponent<PlayerInventory> ();
+		value = 1;
+		//onTriggerStay ();
+		//print("Cool");
+
+	}
+	
+	void OnTriggerEnter (Collider other)
+	{
+		//print ("Cool2");
+		if(other.gameObject == player)
+		{
+			//print ("Cool3");
+			playerInventory.appleCount = playerInventory.appleCount +value;
+
+			Destroy(gameObject);
+
+			//print(playerInventory.appleCount);
+		}
+	}
+
+}
